94628c14e2b492e9186caac8d5e3f2a8b529eba5
Adebanjo Favour Mayowa <favouradebanjo2@gmail.com>
2026-01-09 21:43:59 +0100

    feat: rm and added the prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,11 +4,5 @@ import "dotenv/config"
 import { defineConfig } from "prisma/config"
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-
-  migrate: {
-    datasource: "db",
-    url: process.env.DATABASE_URL!,
-  },
+  datasourceUrl: process.env.DATABASE_URL,
 })
-
