54d33973968c12357a838bb192c69071740c3343
Adebanjo Favour Mayowa <favouradebanjo2@gmail.com>
2026-02-01 07:41:10 +0100

    feat: Initialize Prisma for database management, including schema definition and client setup.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,17 @@
 import { PrismaClient } from "@prisma/client"
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+const prismaClientSingleton = () => {
+    return new PrismaClient({
+        log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+    })
+}
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClientSingleton | undefined
+}
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
